cd1d36a80491846e2f204ef6bdafa496bf1608e8
ungeskriptet <w.david0@protonmail.com>
2020-11-16 16:21:29 +0100

    discord & fetchcord shortcut

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "Material Design Icons:size=15:antialias=true:autohint=true" };
+static const char *fonts[]          = { "monospace:size=10", "Material Design Icons:size=15:antialias=true:autohint=true", "Noto Color Emoji:pixelsize=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -66,6 +66,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+/* Mod4Mask is used for launching programs, MODKEY is used for dwm functions */
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_F1,     spawn,          {.v = dmenucmd } },
@@ -83,6 +84,7 @@ static Key keys[] = {
 	{ 0,                            XF86XK_AudioMute,spawn,    SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },
 	{ MODKEY,                       XK_s,      spawn,          SHCMD("slock") },
 	{ MODKEY,                       XK_x,      spawn,          SHCMD("xkill") },
+	{ Mod4Mask,                     XK_d,      spawn,          SHCMD("discord & which fetchcord && fetchcord") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
